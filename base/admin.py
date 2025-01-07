1ece73e07a2c031d9e3163eb0f9f563a8fcb0394
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-07 16:41:42 +0200

    Register Category model in admin.py with CategoryAdmin class

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,27 @@
+from base.models import *
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the Category model.
+    """
+
+    list_display = ('name', 'slug', 'created_at', 'updated_at')
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('name',)
+    readonly_fields = ('created_at', 'updated_at')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description')
+        }),
+        (_('SEO'), {
+            'fields': ('slug',)
+        }),
+        (_('Timestamps'), {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
