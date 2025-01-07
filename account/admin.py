28bb84207102739393d16390b6ee0379e9ed8c74
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-07 16:09:44 +0200

    New changes

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,69 @@
-from django.contrib import admin
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+class UserCreationForm(forms.ModelForm):
+    """
+    Form for creating new users. Includes all the required
+    fields, plus a repeated password.
+    """
+    password1 = forms.CharField(
+        label=_('Password'),
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label=_('Password confirmation'),
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'name', 'phone_number', 'role')
+
+    def clean_password2(self):
+        """
+        Validate that both passwords match.
+        """
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(_("Passwords don't match"))
+        return password2
+
+    def save(self, commit=True):
+        """
+        Save the provided password in hashed format.
+        """
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+class UserChangeForm(forms.ModelForm):
+    """
+    Form for updating users. Replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField(
+        label=_("Password"),
+        help_text=_(
+            "Raw passwords are not stored, so there is no way to see this "
+            "user’s password, but you can change the password using "
+            "<a href=\"../password/\">this form</a>."
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'email', 'password', 'name', 'phone_number', 'role',
+            'is_active', 'is_staff', 'slug'
+        )
+
+    def clean_password(self):
+        """
+        Return the initial password value.
+        """
+        return self.initial["password"]
