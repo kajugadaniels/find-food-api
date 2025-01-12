9abcf8a0834eab1bb7970f00a914c599bdbf1fc7
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-13 00:31:32 +0200

    Add function to add new category

--- base/views.py
+++ base/views.py
@@ -13,3 +13,14 @@ class getCategories(APIView):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class addCategory(APIView):
+    """
+    View to create a new Category.
+    """
+    def post(self, request, *args, **kwargs):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
