7835ba549270853efc452f673c80883b230cef3b
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-13 00:30:31 +0200

    Add function to retrieve all categories

--- base/views.py
+++ base/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from base.models import *
+from base.serializers import *
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class getCategories(APIView):
+    """
+    View to list all categories or filter them if needed in the future.
+    """
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
